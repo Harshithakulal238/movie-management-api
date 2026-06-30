708adb131794789edbcf08fc7c3be0febcd12e0d
Harshitha Kulal <harshitha.23cs046@sode-edu.in>
2026-06-30 15:25:33 +0530

    Add GET movies endpoint with sample data

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI
 app=FastAPI()
+movies=[{
+    "id":1,
+    "title":"Inception",
+    "genre":"Sci-Fi",
+    "rating":8.8
+    },
+    {
+        "id":2,
+        "title":"The Dark Knight",
+        "genre":"Action",
+        "rating":9.0
+    }]
 @app.get("/")
 def home():
     return {"message": "Welcome to the Movie Management API!"}
+
+@app.get("/movies")
+def get_movies():
+    return  movies
+
+
